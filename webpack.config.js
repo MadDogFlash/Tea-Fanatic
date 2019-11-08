3494d1be2aabfa86096faef58d72fb7d09af871d
MadDogFlash <49573648+MadDogFlash@users.noreply.github.com>
2019-11-07 22:52:45 -0800

    installed webpack dependencies and config files

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const webpack = require ('webpack');
+const fs      = require ('fs');
+const path    = require ('path'),
+      join    = path.join, 
+      resolve = path.resolve;
+
+const getConfig = require('hjs-webpack');
+
+var config =getConfig({
+    in: join(_dirname, 'src/app,js'),
+    out: join(_dirname, 'dist')
+    clearBeforeBuild: true
+})
