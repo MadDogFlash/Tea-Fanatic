e9fb943e20e355e081241614233b4a7ac4091900
MadDogFlash <49573648+MadDogFlash@users.noreply.github.com>
2019-11-08 00:59:56 -0800

    removed Vue depencies and added React, ReactDOM and other peer dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,42 @@ const path    = require ('path'),
 
 const getConfig = require('hjs-webpack');
 
-var config =getConfig({
-    in: join(_dirname, 'src/app,js'),
-    out: join(_dirname, 'dist')
+var config = getConfig({
+    in: join(__dirname, 'src/app,js'),
+    out: join(__dirname, 'dist')
     clearBeforeBuild: true
 })
+
+
+const root    = resolve(__dirname);
+const src     = join(root, 'src');
+const modules = join(root, 'node_modules');
+const dest    = join(root, 'dist');
+
+var config = getConfig({
+  in: join(src, 'app.js'),
+  out: dest,
+  clearBeforeBuild: true
+})
+
+const NODE_ENV = process.env.NODE_ENV;
+const isDev = NODE_ENV === 'development';
+// alternatively, we can use process.argv[1]
+// const isDev = (process.argv[1] || '')
+//                .indexOf('hjs-dev-server') !== -1;
+
+// ...
+var config = getConfig({
+  isDev: isDev,
+  in: join(src, 'app.js'),
+  out: dest,
+  clearBeforeBuild: true
+})
+
+
+// ...
+var config = getConfig({
+    // ...
+  })
+  
+  module.exports = config;
